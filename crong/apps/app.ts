c551ed58627753091b55947dbb44b032ec8b26a9
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-13 11:32:11 +0900

    feat: passport based router configuration

--- crong/apps/app.ts
+++ crong/apps/app.ts
@@ -5,13 +5,17 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 const router = require("./router/index");
+const localStorage = require('passport-local').Strategy;
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
 
 export const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'codesquad',
-  database: 'nodetest'
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'codesquad',
+    database: 'nodetest'
 });
 
 connection.connect();
@@ -21,16 +25,26 @@ app.use(router);
 app.use(bodyParser.json());
 
 app.listen(3006, function () {
-  console.log("Example app listening on port 3006!");
+    console.log("Example app listening on port 3006!");
 });
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 );
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
--- crong/apps/router/index.ts
+++ crong/apps/router/index.ts
@@ -4,13 +4,30 @@ const router = express.Router();
 const main = require("./main");
 const email = require("./email");
 const join = require("./join");
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 router.use('/main', main);
 router.use('/email', email);
 router.use('/join', join);
 
 router.get("/", function (request: express.Request, response: express.Response) {
-    response.send("Hello World!");
+    response.render('join.ejs');
 });
+
+router.post('/', passport.authenticate('local-join', {
+    successRedirect: '/main',
+    failureRedirect: '/join',
+    failureFlash: true
+}));
+
+passport.use('local-join', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, function (req: express.Request, email: String, password: String, done: boolean) {
+        console.log('local-join callback called');
+    }
+));
 
 module.exports = router;
